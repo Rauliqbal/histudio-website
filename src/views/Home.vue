3ad68a841d557bdfddee0fcc0c8f894ca2b9f07f
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2022-12-01 11:49:02 +0700

    add loading product

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,10 @@
       <Category id="category" />
       <div class="container py-20">
          <h3 class="section-title mb-8">Featured Projects</h3>
-         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+         <div v-if="products.length < 1">
+            <img class="w-16 h-16 mx-auto animate-spin" src="../assets/images/loading.png" alt="" />
+         </div>
+         <div v-else class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             <div v-for="product in products" :key="product.id">
                <ProductCard :product="product" />
             </div>
